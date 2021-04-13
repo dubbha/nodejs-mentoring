56ad6f17b7f65ce6641c5a283eae5513e2d301b6
Oleksiy Dubovyk <dubovyk@gmail.com>
2021-04-14 00:05:34 +0300

    hw3 init

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
 import { LoginNotUsedConstraint } from './validators/login-not-used';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, LoginNotUsedConstraint],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,43 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Not } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
 
   create(createUserDto: CreateUserDto) {
-    this.users.push({
-      ...createUserDto,
-      id: uuidv4(),
-      isDeleted: false,
-    });
+    return this.usersRepository.save(createUserDto);
   }
 
-  findAll(loginSubstring?: string, limit?: number) {
-    return this.users
-      .filter(({ isDeleted }) => !isDeleted)
-      .map(({ isDeleted, ...rest }) => ({ ...rest })) // eslint-disable-line @typescript-eslint/no-unused-vars
+  async findAll(loginSubstring?: string, limit?: number) {
+    return (await this.usersRepository.find({ deletedAt: null }))
+      .map(({ deletedAt, ...rest }) => ({ ...rest })) // eslint-disable-line @typescript-eslint/no-unused-vars
       .filter(({ login }) => login.match(new RegExp(loginSubstring, 'i')))
       .sort((a, b) => (a.login > b.login ? 1 : -1))
       .slice(0, limit);
   }
 
   findOne(id: string) {
-    return this.users.find(user => user.id === id);
+    return this.usersRepository.findOne({ id, deletedAt: null });
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    this.users = this.users.map(user => (user.id === id ? { ...user, ...updateUserDto } : user));
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    await this.usersRepository.update({ id }, updateUserDto);
   }
 
-  remove(id: string) {
-    this.users = this.users.map(user => (user.id === id ? { ...user, isDeleted: true } : user));
+  async remove(id: string) {
+    await this.usersRepository.softDelete(id);
   }
 
   findOneByLogin(login: string, exceptId?: string): Promise<User> {
-    return Promise.resolve(this.users.find(user => user.login === login && user.id !== exceptId));
+    return this.usersRepository.findOne({
+      login,
+      deletedAt: null,
+      ...(exceptId && { id: Not(exceptId) }),
+    });
   }
 }
